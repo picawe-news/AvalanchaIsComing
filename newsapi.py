0d47b81eabe1390ec427b2543a6f5c1f953a46c6
KMicha <michael.kahle@yahoo.de>
2025-11-21 15:16:24 +0100

    Update newsapi.py

--- newsapi.py
+++ newsapi.py
@@ -169,7 +169,7 @@ def storeCollection():
     global collectedNews
     print("Inside store")
     #cols = ['url','valid','domain','title','description','image','published','archive','content','quote','language','keyword']
-    cols = ['published', 'keyword', 'domain', 'language', 'valid', 'de', 'en', 'la', 'title', 'description', 'url', 'image', 'archive', 'content', 'quote', 'added']
+    cols = ['published', 'keyword', 'domain', 'language', 'valid', 'de', 'title', 'description', 'en', 'la', 'url', 'image', 'archive', 'content', 'quote', 'added']
     for dateFile in collectedNews:
         df = pd.DataFrame.from_dict(collectedNews[dateFile], orient='index', columns=cols)
         df.index = df['url'].apply( lambda x: hashlib.sha256(x.encode()).hexdigest()[:32])   
